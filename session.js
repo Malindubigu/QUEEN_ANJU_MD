c610ba24f778f0b821dfb567159ebbc3478eaaef
Malindubigu <Malimdubigusara@gmail.com>
2024-11-30 10:56:23 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=DQsV3LIa#dm-gnjakLtgOFaDm985oKUnxCUdPPOYrUJJripglfmA",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=vFNRzCDA#ixb4VHcJEohal3qTrfyGrobDQqh-KfhroEwDgsdzVpQ",
 };
